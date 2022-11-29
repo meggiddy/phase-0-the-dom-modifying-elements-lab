518a22f67783f6a00ef48cb2ccfe01357a75d81d
Maggie <muriithinyawira13@gmail.com>
2022-11-29 13:21:36 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const removeMain = document.getElementById("main");
+    removeMain.remove();
+
+const newHeader = document.createElement('h1');
+    newHeader.setAttribute('id', 'victory')
+    newHeader.textContent = 'Meg is the champion';
+    body.append(newHeader);
